e65887461524489ca4d83026edb161cb09be55f5
BCvuongvan <vuongvann006@gmail.com>
2025-05-09 10:48:52 +0700

    Fix image service
    fix image service

---
+++ src/TraVinhMaps.Application/External/Models/CloudinarySetting.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraVinhMaps.Application.External.Models;
+public class CloudinarySetting
+{
+    public string CloudName { get; set; }
+    public string ApiKey { get; set; }
+    public string ApiSecret { get; set; }
+}
