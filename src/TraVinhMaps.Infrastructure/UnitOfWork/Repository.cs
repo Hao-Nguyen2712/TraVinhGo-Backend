f2d03160b0a906348952fa0424ec31f207969f19
DuongTanDat <datdtce171751@fpt.edu.vn>
2025-05-24 23:04:10 +0700

    refactor(infrastructure): update repositories
    - Refactored UserRepository for cleaner logic or extended functionality
    - Updated generic Repository logic in UnitOfWork layer

--- src/TraVinhMaps.Infrastructure/UnitOfWork/Repository.cs
+++ src/TraVinhMaps.Infrastructure/UnitOfWork/Repository.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Linq.Expressions;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using TraVinhMaps.Application.UnitOfWorks;
 using TraVinhMaps.Domain.Entities;
@@ -48,8 +49,8 @@ public class Repository<T> : IRepository<T> where T : BaseEntity
 
     public async Task<T> GetByIdAsync(string id, CancellationToken cancellationToken = default)
     {
-        // var filter = Builders<T>.Filter.Eq(e => e.Id, id);
-        var filter = Builders<T>.Filter.Eq("_id", id);
+         var filter = Builders<T>.Filter.Eq(e => e.Id, id);
+        //var filter = Builders<T>.Filter.Eq("_id", id);
         return await _collection.Find(filter).FirstOrDefaultAsync(cancellationToken);
     }
 
