868b0068b341aa8cdd7ede62ee386aa72774c404
To.Ly.My <148930434+MyCode-MyLove@users.noreply.github.com>
2025-06-20 19:32:18 +0700

    handle-selling-link
    handle selling link and change attribute name in selling link model

---
+++ src/TraVinhMaps.Application/Features/OcopProduct/Models/UpdateSellLocationRequest.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraVinhMaps.Domain.Entities;
+
+namespace TraVinhMaps.Application.Features.OcopProduct.Models;
+public class UpdateSellLocationRequest
+{
+    public string Id { get; set; }
+    public string? LocationName { get; set; }
+    public string? LocationAddress { get; set; }
+    public string? MakerId { get; set; }
+    public Location? Location { get; set; }
+}
